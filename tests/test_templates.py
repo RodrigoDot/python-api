e274dbab9477966262f0c93c4d33383ee9a5c104
rodrigodot <rodrigodot@outlook.com>
2018-07-21 20:48:54 -0300

    First comit

--- tests/test_templates.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-def test_base_template_with_no_context():
-    # base.html should be renderable with no context,
-    # so it can be easily extended.
-    render({}, 'rest_framework/base.html')
